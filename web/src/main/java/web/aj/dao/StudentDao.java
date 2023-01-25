8c8b857d78d2148d338cce4339aa6746fcaad985
Aaditya55 <aadityajoshi291020@gmail.com>
2023-01-25 21:35:12 +0530

    second

--- web/src/main/java/web/aj/dao/StudentDao.java
+++ web/src/main/java/web/aj/dao/StudentDao.java
@@ -12,11 +12,8 @@ public class StudentDao {
 	public void saveUser(Student student) {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
             transaction = session.beginTransaction();
-            // save the student object
             session.save(student);
-            // commit transaction
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
